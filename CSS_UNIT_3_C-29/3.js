8647df0e0b4fb1feec4b1f71abd829616f10df92
Nisarg Jhatakia <40654095+Nisarg000@users.noreply.github.com>
2020-10-06 10:22:34 +0530

    Add files via upload

---
+++ CSS_UNIT_3_C-29/3.js
@@ -0,0 +1,17 @@
+var num1 = 6;
+var num2 = 10;
+
+document.write("The value of the first variable is : " + num1);
+document.write("<br>" + "The value of the second variable is : " + num2 + "<br>");
+
+var add = num1 + num2;
+var sub = num1 - num2;
+var multi = num1 * num2;
+var mod = num1 % num2;
+var div = num1 / num2;
+
+document.write("<br>"+"Addition is: " + add +" <br>" + "<hr>");
+document.write("Multiplication is:" + multi +"<br>"+ "<hr>");
+document.write("Division is:" + div +"<br>"+ "<hr>");
+document.write("Subtraction is:" + sub +"<br>"+ "<hr>");
+document.write("Modulus is:" + mod +"<br>"+ "<hr>");
---
+++ CSS_UNIT_3_C-29/4.js
@@ -0,0 +1,5 @@
+var x = 10;
+var y = 20;
+document.write(x > 0  || y < 0 = true,"<br>"); // true
+document.write(x > 0  && y < 0  = false,"<br>"); // false
+document.write(x != 0 || y < 15 = true,"<br>"); // true
